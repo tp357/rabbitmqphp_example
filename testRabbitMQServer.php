afa009caf8a05753d9c4c853bd1f93e99e8ae48f
tp357 <tp357@njit.edu>
2025-10-20 12:22:25 -0400

    login and register functions incomplete

--- testRabbitMQServer.php
+++ testRabbitMQServer.php
@@ -4,14 +4,26 @@ require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 
+// Login Function takes the username and password from the form/appache/front end which is stored in variables and then passed to the function where the variable userlookip is called/run on the sql database which selects id, username, and password colums from the users table where the username matches the passed data from the form in the username variable, if it matches it returns true if it doesnt it returns false.
 function doLogin($username,$password)
 {
-    // lookup username in databas
-    // check password
-    return true;
+	// lookup username in databas
+	$userlookup = "SELECT id,username, password FROM users WHERE username = '$username' ";
+	// check password
+	if ($password == 'password') {	
+    return true; }
     //return false if not valid
+}else {
+	return false;
 }
 
+// Registration Function
+function doRegister($username,$password) {
+	// when called the function runs/passes a sql statement that inserts into the users table in the username and password columns the username and password data from the $username and $password variables which will come from the form and frontend/appache.
+$registerUser = "INSERT INTO users (username, password) VALUES ('$username, $password)";
+
+
+}
 function requestProcessor($request)
 {
   echo "received request".PHP_EOL;
