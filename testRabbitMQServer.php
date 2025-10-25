b4c8bf5963f0dafcc21fbddf6a5197d07ec3ae18
tp357 <tp357@njit.edu>
2025-10-25 14:53:18 -0400

    db connection complete, switch statement updated(request type), functions created for register, login, and validate - in progress

--- testRabbitMQServer.php
+++ testRabbitMQServer.php
@@ -4,26 +4,37 @@ require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 
+$dbConnect = new mysqli("tirth-VMware20-1","backrabbituser",'backpassword','testingdb');
+  	      	
 // Login Function takes the username and password from the form/appache/front end which is stored in variables and then passed to the function where the variable userlookip is called/run on the sql database which selects id, username, and password colums from the users table where the username matches the passed data from the form in the username variable, if it matches it returns true if it doesnt it returns false.
-function doLogin($username,$password)
-{
-	// lookup username in databas
-	$userlookup = "SELECT id,username, password FROM users WHERE username = '$username' ";
-	// check password
-	if ($password == 'password') {	
-    return true; }
-    //return false if not valid
-}else {
-	return false;
-}
+
 
 // Registration Function
 function doRegister($username,$password) {
+	global $dbConnect;
 	// when called the function runs/passes a sql statement that inserts into the users table in the username and password columns the username and password data from the $username and $password variables which will come from the form and frontend/appache.
-$registerUser = "INSERT INTO users (username, password) VALUES ('$username, $password)";
+	$registerUser = "INSERT INTO users (username, password) VALUES ('$username, $password)";
 
 
 }
+
+function doLogin($username,$password) {
+	global $dbConnect;
+// username database lookup
+	$userLookup = "SELECT id,username, password FROM users WHERE username = '$username' ";
+// check password
+	if ($pasword == 'passowrd') {
+		return true;
+	}else {
+		return false;
+	}
+}
+
+function doValidate($sessionKey) {
+	global $dbConnect;
+	$validateQuery = "SELECT username FROM sessions WHERE session_key = '$sessionKey'";
+}
+
 function requestProcessor($request)
 {
   echo "received request".PHP_EOL;
@@ -34,6 +45,8 @@ function requestProcessor($request)
   }
   switch ($request['type'])
   {
+	case "register":
+		return doRegister($request['username'],$request['password']);
     case "login":
       return doLogin($request['username'],$request['password']);
     case "validate_session":
